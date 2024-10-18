720271a3639e6a7d72fbe4aead0d865ff0d4b257
SiposZoltan63 <siposz@kkszki.hu>
2024-10-18 12:29:03 +0200

    kesz

--- Program.cs
+++ Program.cs
@@ -39,37 +39,6 @@ namespace feladat
             }
             Console.ReadKey();
 
-            // Lista a pozitív és negatív számok tárolására
-            List<int> pozitivSzamok = new List<int>();
-            List<int> negativSzamok = new List<int>();
-
-            int pozitivSzamokOsszege = 0;
-            int negativSzamokOsszege = 0;
-
-            // 10 szám bekérése
-            for (int i = 0; i < 10; i++)
-            {
-                Console.Write($"Kérlek, adj meg egy {i + 1} egész számot: ");
-                int szamok = Convert.ToInt32(Console.ReadLine());
-
-                if (szamok >= 0)
-                {
-                    pozitivSzamok.Add(szamok);
-                    pozitivSzamokOsszege += szamok;
-                }
-                else
-                {
-                    negativSzamok.Add(szamok);   
-                    negativSzamokOsszege += szamok;
-                }
-            }
-            Console.WriteLine(pozitivSzamok);
-            Console.WriteLine("Pozitív számok összege: " + pozitivSzamokOsszege);
-
-            Console.WriteLine(negativSzamok);
-            Console.WriteLine("Negatív számok összege: " + negativSzamokOsszege);
-
-            Console.ReadKey();
         }
     }
 }
