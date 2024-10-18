ce9009cd940b5afad5a46217a654859c5162a0a7
SiposZoltan63 <siposz@kkszki.hu>
2024-10-18 11:13:12 +0200

    elso feladat elkeszult

--- Program.cs
+++ Program.cs
@@ -10,7 +10,34 @@ namespace feladat
     {
         static void Main(string[] args)
         {
-
+            //első feladat
+            Console.WriteLine("Add meg a pontszámott (0-100) között: ");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            if (-1 >= szam && 100 < szam)
+            {
+                Console.WriteLine("Érvénytelen pontszám");
+            }
+            else if (50 > szam && 0 <= szam)
+            {
+                Console.WriteLine("Ez a dolgozat Elégtelen");
+            }
+            else if (60 > szam && 50 <= szam)
+            {
+                Console.WriteLine("Ez a dolgozat Elégséges");
+            }
+            else if (80 > szam && 60 <= szam)
+            {
+                Console.WriteLine("Ez a dolgozat Közepes");
+            }
+            else if (90 > szam && 80 <= szam)
+            {
+                Console.WriteLine("Ez a dolgozat Jó");
+            }
+            else if (101 > szam && 90 <= szam)
+            {
+                Console.WriteLine("Ez a dolgozat Jeles");
+            }
+            Console.ReadKey();
         }
     }
 }
